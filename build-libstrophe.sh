e7519ecb9d8217e33ef3279f15d6b8feeddcf06f
Chris A. Taylor <chris@gameclosure.com>
2013-10-09 11:03:46 -0700

    Add expat to build and first working build

--- build-libstrophe.sh
+++ build-libstrophe.sh
@@ -26,10 +26,17 @@ git clone git@github.com:strophe/libstrophe.git
 
 cp "${CURRENTPATH}/tls_securetransport.c" "${CURRENTPATH}/libstrophe/src/"
 
+cp -R "${CURRENTPATH}/expat/" "${CURRENTPATH}/libstrophe/src/"
+
+cp "${CURRENTPATH}/Makefile.am" "${CURRENTPATH}/libstrophe/"
+
 cd "${CURRENTPATH}/libstrophe"
 
-sed -ie "s/-lssl -lcrypto -lz/-lz/g" "Makefile.am"
-sed -ie "s/tls_openssl.c/tls_securetransport.c/g" "Makefile.am"
+#sed -ie "s/-lssl -lcrypto -lz/-lz/g" "Makefile.am"
+#sed -ie "s/tls_openssl.c/tls_securetransport.c/g" "Makefile.am"
+sed -ie "s/AC_CHECK_HEADER(openssl\/ssl.h/#AC_CHECK_HEADER(sslblahblah/g" "configure.ac"
+sed -ie "s/AC_CHECK_HEADER(expat.h/AC_CHECK_HEADER(stdlib.h/g" "configure.ac"
+sed -ie "s/#include <expat.h>/#include \"expat.h\"/g" "src/parser_expat.c"
 
 ./bootstrap.sh
 
@@ -72,24 +79,23 @@ echo "Please stand by..."
 
 # /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang
 
-export CC="${DEVELOPER}/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang -arch ${ARCH}"
 mkdir -p "${CURRENTPATH}/bin/${PLATFORM}${SDKVERSION}-${ARCH}.sdk"
 
 echo "Configure ${PACKAGE} for ${PLATFORM} ${SDKVERSION} ${ARCH}"
 
-./configure BSD-generic32
+CC="${DEVELOPER}/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang -arch ${ARCH} -isysroot ${DEVELOPER}/Platforms/${PLATFORM}.platform/Developer/SDKs/${PLATFORM}${SDKVERSION}.sdk -arch ${ARCH} -pipe -Os -gdwarf-2 -miphoneos-version-min=${SDKVERSION}" ./configure --host=${ARCH}-apple-darwin
 
-sed -ie "s!^CFLAG=!CFLAG=-isysroot ${DEVELOPER}/Platforms/${PLATFORM}.platform/Developer/SDKs/${PLATFORM}${SDKVERSION}.sdk !" "Makefile"
-sed -ie "s!^CFLAG=!CFLAG=-miphoneos-version-min=${SDKVERSION} !" "Makefile"
+#sed -ie "s!^CFLAG=!CFLAG=-isysroot ${DEVELOPER}/Platforms/${PLATFORM}.platform/Developer/SDKs/${PLATFORM}${SDKVERSION}.sdk !" "Makefile"
+#sed -ie "s!^CFLAG=!CFLAG=-miphoneos-version-min=${SDKVERSION} !" "Makefile"
 # remove sig_atomic for iPhoneOS
 #sed -ie "s!static volatile sig_atomic_t intr_signal;!static volatile intr_signal;!" "crypto/ui/ui_openssl.c"
 
 echo "Make ${PACKAGE} for ${PLATFORM} ${SDKVERSION} ${ARCH}"
 
-make libstrophe.a
+make -j libstrophe.a
 #make install
-cp "${CURRENTPATH}/libstrophe/libstrophe.a" "${CURRENTPATH}/bin/${PLATFORM}${SDKVERSION}.sdk/"
-cp "${CURRENTPATH}/libstrophe/strophe.h" "${CURRENTPATH}/bin/${PLATFORM}${SDKVERSION}.sdk/"
+cp "${CURRENTPATH}/libstrophe/libstrophe.a" "${CURRENTPATH}/bin/${PLATFORM}${SDKVERSION}-${ARCH}.sdk/"
+cp "${CURRENTPATH}/libstrophe/strophe.h" "${CURRENTPATH}/bin/${PLATFORM}${SDKVERSION}-${ARCH}.sdk/"
 make clean
 
 echo "Building ${PACKAGE} for ${PLATFORM} ${SDKVERSION} ${ARCH}, finished"
@@ -107,19 +113,19 @@ mkdir -p "${CURRENTPATH}/bin/${PLATFORM}${SDKVERSION}-${ARCH}.sdk"
 
 echo "Configure ${PACKAGE} for ${PLATFORM} ${SDKVERSION} ${ARCH}"
 
-./configure BSD-generic32
+CC="${DEVELOPER}/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang -arch ${ARCH} -isysroot ${DEVELOPER}/Platforms/${PLATFORM}.platform/Developer/SDKs/${PLATFORM}${SDKVERSION}.sdk -arch ${ARCH} -pipe -Os -gdwarf-2 -miphoneos-version-min=${SDKVERSION}" ./configure --host=${ARCH}-apple-darwin
 
-sed -ie "s!^CFLAG=!CFLAG=-isysroot ${DEVELOPER}/Platforms/${PLATFORM}.platform/Developer/SDKs/${PLATFORM}${SDKVERSION}.sdk !" "Makefile"
-sed -ie "s!^CFLAG=!CFLAG=-miphoneos-version-min=${SDKVERSION} !" "Makefile"
+#sed -ie "s!^CFLAG=!CFLAG=-isysroot ${DEVELOPER}/Platforms/${PLATFORM}.platform/Developer/SDKs/${PLATFORM}${SDKVERSION}.sdk !" "Makefile"
+#sed -ie "s!^CFLAG=!CFLAG=-miphoneos-version-min=${SDKVERSION} !" "Makefile"
 # remove sig_atomic for iPhoneOS
 #sed -ie "s!static volatile sig_atomic_t intr_signal;!static volatile intr_signal;!" "crypto/ui/ui_openssl.c"
 
 echo "Make ${PACKAGE} for ${PLATFORM} ${SDKVERSION} ${ARCH}"
 
-make libstrophe.a
+make -j libstrophe.a
 #make install
-cp "${CURRENTPATH}/libstrophe/libstrophe.a" "${CURRENTPATH}/bin/${PLATFORM}${SDKVERSION}.sdk/"
-cp "${CURRENTPATH}/libstrophe/strophe.h" "${CURRENTPATH}/bin/${PLATFORM}${SDKVERSION}.sdk/"
+cp "${CURRENTPATH}/libstrophe/libstrophe.a" "${CURRENTPATH}/bin/${PLATFORM}${SDKVERSION}-${ARCH}.sdk/"
+cp "${CURRENTPATH}/libstrophe/strophe.h" "${CURRENTPATH}/bin/${PLATFORM}${SDKVERSION}-${ARCH}.sdk/"
 make clean
 
 echo "Building ${PACKAGE} for ${PLATFORM} ${SDKVERSION} ${ARCH}, finished"
@@ -135,8 +141,6 @@ lipo -create ${CURRENTPATH}/bin/iPhoneSimulator${SDKVERSION}.sdk/libstrophe.a ${
 mkdir -p ${CURRENTPATH}/include
 cp ${CURRENTPATH}/bin/iPhoneSimulator${SDKVERSION}.sdk/strophe.h ${CURRENTPATH}/include/
 echo "Building done."
-
-exit;
 
 echo "Cleaning up..."
 rm -rf ${CURRENTPATH}/src
